c12c3a9be33b1b7d892810fc605526815c2beea2
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2024-04-05 21:15:43 +0200

    Used Basic Cookies.

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,15 @@ module.exports.loginProcess = loginProcess
 module.exports.chat = chat
 
 function index(req, res) {
-    res.render('index', {title: 'index'})
+    res.cookie('Index Cookies', 'This is Set from Index.JS from Back-End!');
+    res.render('index', {
+        title: 'index', 
+        cookie: JSON.stringify(req.cookies)
+    });
 }
 
 function login(req, res) {
-    res.render('login', {title: 'login', body: 'body'});
+    res.render('login', { title: 'login', body: 'body' });
 }
 
 function loginProcess(req, res) {
@@ -16,5 +20,5 @@ function loginProcess(req, res) {
 }
 
 function chat(req, res) {
-    res.render('chat', {title: 'chat', body: 'chat'});
+    res.render('chat', { title: 'chat', body: 'chat' });
 }
